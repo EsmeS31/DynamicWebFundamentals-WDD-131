e69993424153ae980e6c5f89672c9f7afcf6b98b
EsmeS31 <san25011@byui.edu>
2025-09-30 10:07:04 -0600

    changes images

---
+++ Sandbox/arrays.js
@@ -0,0 +1,7 @@
+//  arrays.js
+const steps = ["one", "two", "three"];
+function listTemplate(step) {
+  return `<li>${step}</li>`;
+}
+const stepsHtml = steps.map
+document.querySelector("#myList").innerHTML = stepsHtml.join("");
